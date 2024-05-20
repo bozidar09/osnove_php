b62078ecb6d49d51861112b3d851f79e10dcf09e
bozidar <bozidar.markovic09@mail.com>
2024-05-20 17:40:35 +0200

    vježba

---
+++ uvod_php/obrazac.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Obrazac</title>
+</head>
+<body>
+<table border="1">
+        <tr>
+            <th align="left">Ime:</th>
+            <td>
+                <input type="text" name="first_name" id="first_name">
+            </td>
+        </tr>
+        <tr>
+            <th align="left">Prezime:</th>
+            <td>
+                <input type="text" name="surname" id="surname">
+            </td>
+        </tr>
+        <tr>
+            <th align="left">Email:</th>
+            <td>
+                <input type="text" name="email" id="email">
+            </td>
+        </tr>
+        <tr>
+            <th align="left">Lozinka:</th>
+            <td>
+                <input type="text" name="password" id="password">
+            </td>
+        </tr>
+        <tr>
+            <th colspan="2" align="left"><button type="submit" width="200px">Pošalji!</button>
+            </th>
+        </tr>
+    </table>
+</body>
+</html>
---
+++ uvod_php/tipovi_podataka_vjezba.php
@@ -0,0 +1,36 @@
+<?php
+
+    // brojevi
+    $cijeli_broj = 4;
+    $realni_broj = 2.99;
+    $tekstualan_podatak = "neki tekst";
+    $logicka_vrijednost = false;
+
+    echo $cijeli_broj;
+    echo "<br>";
+    echo $realni_broj;
+    echo "<br>";
+    echo $tekstualan_podatak;
+    echo "<br>";
+    echo $logicka_vrijednost? "true":"false";
+    echo "<br>";
+
+    // konstante
+    define("ABSOLUTE_ZERO", "−273.15");
+    const IBAN = "HR5902589275020952";
+
+    echo ABSOLUTE_ZERO;
+    echo "<br>";
+    echo IBAN;
+    echo "<br>";
+
+    // reference
+    $a = 5;
+    $b = &$a;
+
+    echo "Prije promjene vrijednosti a " . $b;
+    echo "<br>";
+    
+    $a = 6;
+    echo "Nakon promjene vrijednosti a " . $b; // vrijednost b je 6
+
