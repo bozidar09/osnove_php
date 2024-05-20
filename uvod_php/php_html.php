ddf4b77b6a90cd151d5abac1d11f00a85a057511
bozidar <bozidar.markovic09@mail.com>
2024-05-20 15:31:54 +0200

    drugi commit

--- uvod_php/php_html.php
+++ uvod_php/php_html.php
@@ -7,6 +7,33 @@
 </head>
 <body>
     <h1>PHP & HTML</h1>
-    
+
+    <?php 
+        // ini_set ("error_reporting", E_ALL);
+        // ini_set ("display_errors", 1);
+
+        // include "include_require.php555";
+        require_once "include_require.php444";
+
+        $ispis = "Ovo je ispisano pomoću PHP jezika"
+    ?>
+
+    <section>
+        <p>
+            <?php 
+                echo $ispis 
+            ?>
+        </p>
+    </section>
+
+    <section>
+        <p>
+            <?= $ispis2 ?>
+        </p>
+        <p>
+            <?= "Skraćeni PHP tag za ispis" ?>
+        </p>
+    </section>
+
 </body>
 </html>
