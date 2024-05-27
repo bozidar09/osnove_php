d1af1583d25f9ae0ae9874ea40cee9393c41c8ba
bozidar <bozidar.markovic09@mail.com>
2024-05-27 23:44:54 +0200

    funkcije vježba

--- 6.funkcije/funkcije_vjezba.php
+++ 6.funkcije/funkcije_vjezba.php
@@ -1,3 +1,42 @@
 <?php
 
-    
+    // vježba 1
+
+    function textReturn(): string
+    {
+        return "neki tekst";
+    }
+
+    $text = textReturn();
+    echo $text;
+    echo "<br>";
+
+
+    // vježba 2
+
+    function concatenate(string $name, string $surname): string
+    {
+        $fullName = "$name $surname";
+        return mb_strtoupper($fullName);
+    }
+
+    $namePrint = concatenate("božidar", "marković");
+    echo $namePrint;
+    echo "<br>";
+
+
+    // vježba 3
+
+    function randSum(int $number): int
+    {
+        static $sum = 0;
+        $sum += $number;
+        return $sum;
+    }
+
+    $sumFunc = "randSum";
+
+    for ($i=1; $i<=5; $i++) {
+        echo $sumFunc(rand(1,10));
+        echo "<br>";
+    }
