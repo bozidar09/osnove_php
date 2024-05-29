cbca0bd5fdd71bb520f3fcfdafc45fbaafa9805e
bozidar <bozidar.markovic09@mail.com>
2024-05-29 02:25:53 +0200

    dodana funkcija

--- 7.datoteke/datoteke_vjezba.php
+++ 7.datoteke/datoteke_vjezba.php
@@ -9,7 +9,9 @@
     // sudo chmod 664 ./podaci/polaznici.json  ->  dodavanje korisnicima u vlastitoj grupi write ovlasti nad datotekom polaznici.json
     // sudo systemctl reload apache2  ->  restart apache servera
 
+
     const FILE_PATH = __DIR__ . "/podaci/polaznici.json";
+
 
     // funkcija dohvaća podatke iz datoteke i pretvara ih u array
     function getDecode(string $filePath): array
@@ -17,13 +19,24 @@
         return json_decode(file_get_contents($filePath), true);
     }
     
+
     // funkcija pretvara podatke u json tip i sprema ih u datoteku
     function encodePut(array $newData, string $filePath): void
     {
         file_put_contents($filePath, json_encode($newData));
     }
-    
+
+
+    // funkcija za brisanje zadnjeg elementa iz json datoteke (sa ciljem kako bi se prva i druga tablica razlikovale nakon reloada stranice) 
+    function getPopPut(string $filePath): void
+    {
+        $tempArray = getDecode($filePath);
+        array_pop($tempArray);
+        encodePut($tempArray, $filePath);
+    }
+
 ?>
+
 
 <!DOCTYPE html>
     <html lang="en">
@@ -73,7 +86,7 @@
                     "email" => "borna.boric@gmail.com",
                     "phone" => 38595666777
                 ];
-                $students = array_unique($students, SORT_REGULAR); // funkcija koja rješava problem duplikata
+                $students = array_unique($students, SORT_REGULAR);  // ugrađena php funkcija koja rješava problem duplikata
                 encodePut($students, FILE_PATH);
                 $students = getDecode(FILE_PATH);
                 $key = array_key_first($students);
@@ -96,6 +109,9 @@
                 <?php endforeach; ?>
             </tbody>
         </table>
-
+    
     </body>
 </html>
+
+<?php getPopPut(FILE_PATH); ?>
+    
